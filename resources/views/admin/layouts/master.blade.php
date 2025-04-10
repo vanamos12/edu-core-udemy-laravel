974ff258ef796bf32805eda20a16f3c1f488b5d7
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2025-04-10 16:57:41 +0100

    refactor first github action.

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -15,9 +15,6 @@
     <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
     <!-- CSS files -->
     <link href="{{ asset('admin/assets/dist/css/tabler.min.css?1692870487') }}" rel="stylesheet"/>
-    <link href="{{ asset('admin/assets/dist/css/tabler-flags.min.css?1692870487') }}" rel="stylesheet"/>
-    <link href="{{ asset('admin/assets/dist/css/tabler-payments.min.css?1692870487') }}" rel="stylesheet"/>
-    <link href="{{ asset('admin/assets/dist/css/tabler-vendors.min.css?1692870487') }}" rel="stylesheet"/>
     <link href="{{ asset('admin/assets/dist/css/demo.min.css?1692870487') }}" rel="stylesheet"/>
     <style>
       @import url('https://rsms.me/inter/inter.css');
@@ -2252,14 +2249,9 @@
         </div>
       </div>
     </div>
-    <!-- Libs JS -->
-    <script src="{{ asset('admin/assets/dist/libs/apexcharts/dist/apexcharts.min.js?1692870487') }}" defer></script>
-    <script src="{{ asset('admin/assets/dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1692870487') }}" defer></script>
-    <script src="{{ asset('admin/assets/dist/libs/jsvectormap/dist/maps/world.js?1692870487') }}" defer></script>
-    <script src="{{ asset('admin/assets/dist/libs/jsvectormap/dist/maps/world-merc.js?1692870487') }}" defer></script>
     <!-- Tabler Core -->
     <script src="{{ asset('admin/assets/dist/js/tabler.min.js?1692870487') }}" defer></script>
     <script src="{{ asset('admin/assets/dist/js/demo.min.js?1692870487') }}" defer></script>
-   
+    
   </body>
 </html>
